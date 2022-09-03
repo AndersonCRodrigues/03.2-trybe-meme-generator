cd7bfef3e864a40e0ad1e3db1c08a3873422968e
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-03 10:25:15 -0300

    adiciona requisito 06

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const divMeme = document.querySelector('#meme-image-container');
 const textMeme = document.querySelector('#meme-text');
 const inputImg = document.querySelector('#meme-insert');
 const memeImg = document.querySelector('#meme-image');
+const fireBtn = document.querySelector('#fire');
+const waterBtn = document.querySelector('#water');
+const earthBtn = document.querySelector('#earth');
 
 inputText.addEventListener('keyup', (event) => {
   if (event.value !== '') textMeme.innerText = inputText.value;
@@ -13,4 +16,16 @@ inputImg.addEventListener('change', () => {
   if (file) {
     memeImg.src = URL.createObjectURL(file);
   }
+});
+
+fireBtn.addEventListener('click', () => {
+  divMeme.style.border = '3px dashed rgb(255, 0, 0)';
+});
+
+waterBtn.addEventListener('click', () => {
+  divMeme.style.border = '5px double rgb(0, 0, 255)';
+});
+
+earthBtn.addEventListener('click', () => {
+  divMeme.style.border = '6px groove rgb(0, 128, 0)';
 });
